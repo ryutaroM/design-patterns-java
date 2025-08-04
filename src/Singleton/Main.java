e0c8bb927b42fe0cec1dee585de74682835546e3
ryutaroM <arcticmonkey326@gmail.com>
2025-08-04 23:25:48 +0900

    Implement Singleton pattern with Singleton class and Main class

---
+++ src/Singleton/Main.java
@@ -0,0 +1,16 @@
+package Singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start.");
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+
+        if (obj1 == obj2) {
+            System.out.println("obj1 and obj2 are the same instance.");
+        } else {
+            System.out.println("obj1 and obj2 are different instances.");
+        }
+        System.out.println("End.");
+    }
+}
