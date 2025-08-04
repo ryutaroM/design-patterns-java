9746d5193edce9457742384392f2f11cc41690f7
ryutaroM <arcticmonkey326@gmail.com>
2025-08-05 00:13:08 +0900

    Implement Prototype pattern with Manager, Product, UnderlinePen, and MessageBox classes

---
+++ src/Prototype/Main.java
@@ -0,0 +1,26 @@
+package Prototype;
+
+import Prototype.framework.Manager;
+import Prototype.framework.Product;
+
+public class Main {
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+        UnderlinePen upen = new UnderlinePen('-');
+        MessageBox mbox = new MessageBox('*');
+        MessageBox sbox = new MessageBox('/');
+
+        manager.register("strong message", upen);
+        manager.register("warning box", mbox);
+        manager.register("slash box", sbox);
+
+        Product p1 = manager.create("strong message");
+        p1.use("Hello, world.");
+
+        Product p2 = manager.create("warning box");
+        p2.use("Warning: Low disk space!");
+
+        Product p3 = manager.create("slash box");
+        p3.use("Slash box example.");
+    }
+}
---
+++ src/Prototype/MessageBox.java
@@ -0,0 +1,36 @@
+package Prototype;
+
+import Prototype.framework.Product;
+
+public class MessageBox implements Product {
+    private char decoChar;
+
+    public MessageBox(char decoChar) {
+        this.decoChar = decoChar;
+    }
+
+    @Override
+    public void use(String s) {
+        int decolen = 1 + s.length() + 1;
+        for (int i = 0; i < decolen; i++) {
+            System.out.print(decoChar);
+        }
+        System.out.println();
+        System.out.println(decoChar + s + decoChar);
+        for (int i = 0; i < decolen; i++) {
+            System.out.print(decoChar);
+        }
+        System.out.println();
+    }
+
+    @Override
+    public Product createCopy() {
+        Product p = null;
+        try {
+            p = (Product) clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return p;
+    }
+}
