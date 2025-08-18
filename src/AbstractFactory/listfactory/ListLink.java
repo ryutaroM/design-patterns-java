fbbfab99d67a8eaa3eed946bcf1f5cf6fc7b6d1b
ryutaroM <arcticmonkey326@gmail.com>
2025-08-19 00:09:21 +0900

    Implement Abstract Factory pattern with Main, Factory, Item, Link, Page, Tray, ListFactory, ListLink, and ListTray classes

---
+++ src/AbstractFactory/listfactory/ListLink.java
@@ -0,0 +1,13 @@
+package AbstractFactory.listfactory;
+
+public class ListLink extends AbstractFactory.factory.Link {
+    public ListLink(String caption, String url) {
+        super(caption, url);
+    }
+
+    @Override
+    public String makeHTML() {
+        return "  <li><a href=\"" + url + "\">" + caption + "</a></li>\n";
+    }
+
+}
---
+++ src/AbstractFactory/listfactory/ListTray.java
@@ -0,0 +1,23 @@
+package AbstractFactory.listfactory;
+
+import AbstractFactory.factory.Item;
+
+public class ListTray extends AbstractFactory.factory.Tray {
+    public ListTray(String caption) {
+        super(caption);
+    }
+
+    @Override
+    public String makeHTML() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<li>\n");
+        sb.append(caption);
+        sb.append("\n<ul>\n");
+        for (Item item : tray) {
+            sb.append(item.makeHTML());
+        }
+        sb.append("</ul>\n");
+        sb.append("</li>\n");
+        return sb.toString();
+    }
+}
