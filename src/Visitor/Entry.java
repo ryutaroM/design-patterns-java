c1acc59b7336e127919104e51f116c4b6c2a7282
ryutaroM <arcticmonkey326@gmail.com>
2025-10-03 00:00:13 +0900

    Implement Visitor pattern with Directory, File, and ListVisitor classes

---
+++ src/Visitor/Entry.java
@@ -0,0 +1,12 @@
+package Visitor;
+
+public abstract class Entry implements Element {
+    public abstract String getName();
+
+    public abstract int getSize();
+
+    @Override
+    public String toString() {
+        return getName() + " (" + getSize() + ")";
+    }
+}
---
+++ src/Visitor/ListVisitor.java
@@ -0,0 +1,21 @@
+package Visitor;
+
+public class ListVisitor extends Visitor {
+    private String currentDir = "";
+
+    @Override
+    public void visit(File file) {
+        System.out.println(currentDir + "/" + file);
+    }
+
+    @Override
+    public void visit(Directory directory) {
+        System.out.println(currentDir + "/" + directory);
+        String savedir = currentDir;
+        currentDir = currentDir + "/" + directory.getName();
+        for (Entry entry : directory) {
+            entry.accept(this);
+        }
+        currentDir = savedir;
+    }
+}
